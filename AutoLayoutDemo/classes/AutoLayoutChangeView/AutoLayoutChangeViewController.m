439d51f14ffbd022bf45e7101303b6eac23b2e0f
Tb <tb@tbdemacbook-pro.local>
2017-07-02 17:05:13 +0800

    第二个完成

--- AutoLayoutDemo/classes/AutoLayoutChangeView/AutoLayoutChangeViewController.m
+++ AutoLayoutDemo/classes/AutoLayoutChangeView/AutoLayoutChangeViewController.m
@@ -6,9 +6,6 @@
 //  Copyright © 2017年 macTb. All rights reserved.
 //
 
-
-
-
 #import "AutoLayoutChangeViewController.h"
 #import <Masonry.h>
 
@@ -17,18 +14,19 @@
 @property (weak, nonatomic) IBOutlet UIView *bgView;
 
 /**textField*/
-@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIButton *addButton;
 /**textField的bottom的约束*/
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomConstraint;
 
 
-@property (nonatomic, assign)BOOL flage;
+@property (nonatomic, assign)int flage;
 @end
 
 @implementation AutoLayoutChangeViewController
 
 
-static const CGFloat height = 20;
+static const CGFloat kHeight = 30;
+static const CGFloat kMargain = 20;
 
 - (instancetype)initWithTitle:(NSString *)title
 {
@@ -41,35 +39,34 @@ static const CGFloat height = 20;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    
 }
 
 - (void)addView
 {
-    UIView *redView = [[UIView alloc] init];
-    redView.backgroundColor = [UIColor redColor];
-    [self.bgView addSubview:redView];
+    ++self.flage;
+    NSLog(@"flag:%d",self.flage);
     
-    [redView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.textField.mas_bottom).with.offset(height);
-        make.left.right.equalTo(self.bgView);
-        make.height.mas_equalTo(height);
-        
-        make.bottom.equalTo(self.bgView.mas_bottom).with.offset(-height);
+    self.bottomConstraint.constant = self.flage *(kMargain + kHeight) + kMargain;
+    
+    UIImageView *imageView = [[UIImageView alloc] init];
+    [imageView setImage:[UIImage imageNamed:@"send"]];
+    [self.bgView addSubview:imageView];
+    
+    [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.addButton.mas_bottom).with.offset(self.flage *(kMargain + kHeight) + kMargain);
+        make.left.equalTo(self.bgView.mas_left).with.offset(kMargain);
+        make.right.equalTo(self.bgView.mas_right).with.offset(-kMargain);
+        make.height.mas_equalTo(kHeight);
     }];
-    
-   
     
 }
 
 
-- (IBAction)sure:(id)sender
+- (IBAction)add:(id)sender
 {
-    
     [UIView animateWithDuration:0.5 animations:^{
         [self addView];
-
+        [self.bgView setNeedsLayout];
     }];
 }
 
